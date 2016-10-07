8bfd859acf74d3287d64917aff56d7059b26264e
joaobiriba <glaquidara@gmail.com>
2016-10-07 12:25:56 +0200

    Add firebase support to control presentation

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
 var sceneCache = {};
 
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyApQY-Ley5-D5u3zvFmSENla8aUf22nUX0",
+    authDomain: "aframepresent.firebaseapp.com",
+    databaseURL: "https://aframepresent.firebaseio.com",
+    storageBucket: "aframepresent.appspot.com",
+    messagingSenderId: "966908679025"
+  };
+  firebase.initializeApp(config);
+  var ref = firebase.database().ref();
+
 // https://github.com/hakimel/reveal.js#configuration
 Reveal.initialize({
   history: true,
@@ -16,6 +27,14 @@ Reveal.initialize({
   showNotes: false
 });
 
+// 'slidechanged' event listener to update the firebase JSON whenever there is a change in slide.
+// This can be further improved to have firebase authentication to restrict slide update only by the presenter.
+Reveal.addEventListener( 'slidechanged', function( event ) {
+  ref.set({currentslideX : Reveal.getState().indexh,
+         currentslideY : Reveal.getState().indexv
+        })
+});
+
 // Fix Markdown wrapping <img> in <p>.
 fixWrappedImages();
 Reveal.addEventListener('ready', fixWrappedImages);
@@ -29,8 +48,14 @@ function fixWrappedImages () {
     section.insertBefore(img, p);
     section.removeChild(p);
   }
+  console.log("Slide changed event");
 }
 
+// Whenever there is a change in slide position, the same will be updated in the client using Reveal.slide() function .
+ref.on("value", function(snapshot) {
+  Reveal.slide(snapshot.val().currentslideX,snapshot.val().currentslideY);
+  console.log("slide changed received");
+});
 /**
  * Fetch all scenes.
  */
